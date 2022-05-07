a437ca09ea6471310b24e4f0531f3814670a6e47
Iremgunes <aviremdanacioglu@gmail.com>
2022-05-07 14:28:31 +0300

    7 may commit 4

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/JMW3.java
@@ -0,0 +1,53 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JMW3 {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2- Go to: https://google.com
+        driver.get("https://google.com");
+        //3- Click to Gmail from top right.
+        WebElement gmailButton = driver.findElement(By.linkText("Gmail"));
+       gmailButton.click();
+        // 4- Verify title contains:
+        //Expected: Gmail
+
+        String expectedTitle = "Gmail";
+
+        String actualTitle = driver.getTitle();
+        System.out.println("actualTitle = " + actualTitle);
+
+        if(actualTitle.contains(expectedTitle)){
+            System.out.println("passed");
+        }
+
+        Thread.sleep(2000);
+        //5- Go back to Google by using the .back();
+      driver.navigate().back();
+       // 6- Verify title equals:
+       // Expected: Google
+
+        String expectedTitle2 = "Google";
+        String actualNew = driver.getTitle();
+
+        if(actualNew.equals(expectedTitle2)){
+            System.out.println("passed");
+        }else{
+            System.out.println("failed");
+        }
+        Thread.sleep(2000);
+
+        driver.close();
+
+
+    }
+}
