98814bace02d2362feb42b7d983258ce5ca327a6
Iremgunes <aviremdanacioglu@gmail.com>
2022-05-07 11:56:40 +0300

    7 may commit 1

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T5_getText_getAttribute.java
@@ -0,0 +1,43 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T5_getText_getAttribute {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://practice.cydeo.com/registration_form");
+
+        //verify header text is as expected
+        WebElement headerText = driver.findElement(By.tagName("h2"));
+
+        String expectedHeaderText = "Registration form";
+        String actualHeaderText = headerText.getText();
+
+        if(actualHeaderText.equals(expectedHeaderText)){
+            System.out.println("header text verification passed");
+        }else{
+            System.out.println("header text verification) failed");
+        }
+
+        WebElement firstNameInput = driver.findElement(By.name("firstname"));
+
+        String expectedPlaceHolder = "first name";
+
+        String actualPlaceHolder = firstNameInput.getAttribute();
+
+        System.out.println("actualPlaceHolder = " + actualPlaceHolder);
+
+
+
+
+
+    }
+}
