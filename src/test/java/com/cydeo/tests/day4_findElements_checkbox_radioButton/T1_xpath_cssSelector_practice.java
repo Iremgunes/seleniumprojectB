c060a6d693aa342d6d412c9d880c97158414e7df
Iremgunes <aviremdanacioglu@gmail.com>
2022-05-13 16:39:50 +0300

    15 may commit 2

--- src/test/java/com/cydeo/tests/day4_findElements_checkbox_radioButton/T1_xpath_cssSelector_practice.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkbox_radioButton/T1_xpath_cssSelector_practice.java
@@ -21,11 +21,7 @@ public class T1_xpath_cssSelector_practice {
         //3. Locate all the WebElements on the page using XPATH and/or CSS
         //locator only (total of 6)
 
-/*
 
-        4. Verify all web elements are displayed.
-
- */
        // a. “Home” link
         WebElement homeLink_ex1 = driver.findElement(By.cssSelector("a[class = 'nav-link']"));
         WebElement homelink_ex2 = driver.findElement(By.cssSelector("a.nav-link"));
@@ -38,7 +34,7 @@ public class T1_xpath_cssSelector_practice {
         WebElement header_ex2 = driver.findElement(By.xpath("//h2[text()= 'Forgot Password']"));
 
         //c. “E-mail” text
-        WebElement emailLabel_ex1= driver.findElement(By.xpath("//input[@for='email']"));
+        WebElement emailLabel_ex1= driver.findElement(By.xpath("//label[@for='email']"));
 
         //d. E-mail input box
         WebElement inputBox_ex1= driver.findElement(By.xpath("//input[@name='email']"));
@@ -48,11 +44,29 @@ public class T1_xpath_cssSelector_practice {
        // e. “Retrieve password” button
         WebElement retrievePassword_ex1 = driver.findElement(By.cssSelector("button[id='form_submit']"));
         //ex2
-
         WebElement retrievePassword_ex2 = driver.findElement(By.cssSelector("button#form_submit"));
-
+        //ex3
+        WebElement retrievePassword_ex3 = driver.findElement(By.xpath("//button[@id='form_submit']"));
 
        // f. “Powered by Cydeo text
+
+        WebElement poweredByCydeoText = driver.findElement(By.xpath("//div[@style='text-align: center;']"));
+
+        //4. Verify all web elements are displayed.
+
+        System.out.println("homelink_ex1.isDisplayed() = " + homeLink_ex1.isDisplayed());
+
+        System.out.println("header_ex2.isDisplayed() = " + header_ex2.isDisplayed());
+
+        System.out.println("emailLabel_ex1.isDisplayed() = " + emailLabel_ex1.isDisplayed());
+
+        System.out.println("inputBox_ex1.isDisplayed() = " + inputBox_ex1.isDisplayed());
+
+        System.out.println("retrievePassword_ex1.isDisplayed() = " + retrievePassword_ex1.isDisplayed());
+
+        System.out.println("poweredByCydeoText.isDisplayed() = " + poweredByCydeoText.isDisplayed());
+
+
 
 
 
