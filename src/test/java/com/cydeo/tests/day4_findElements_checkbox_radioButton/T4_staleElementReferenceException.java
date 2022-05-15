52a9149208bca0cbbf370adb27e5ff2fedb91855
Iremgunes <aviremdanacioglu@gmail.com>
2022-05-15 13:10:25 +0300

    15 may commit 6

--- src/test/java/com/cydeo/tests/day4_findElements_checkbox_radioButton/T4_staleElementReferenceException.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkbox_radioButton/T4_staleElementReferenceException.java
@@ -1,7 +1,36 @@
 package com.cydeo.tests.day4_findElements_checkbox_radioButton;
 
-public class T4_staleElementReferenceException {
-    public static void main(String[] args) {
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+import java.util.concurrent.TimeUnit;
+
+public class T4_staleElementReferenceException {
+    public static void main(String[] args) throws InterruptedException {
+
+        //1- Open a chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2- Go to: https://practice.cydeo.com/abtest
+        driver.get("https://practice.cydeo.com/abtest");
+
+        //3- Locate “CYDEO” link, verify it is displayed.
+        WebElement cydeoLink = driver.findElement(By.xpath("//a[text()='CYDEO']"));
+
+        System.out.println("cydeoLink.isDisplayed() =" + cydeoLink.isDisplayed());
+
+        Thread.sleep(2000);
+
+        //4- Refresh the page.
+        driver.navigate().refresh();
+
+        //5- Verify it is displayed, again
+
+        cydeoLink = driver.findElement(By.xpath("//a[text()='CYDEO']"));
+        System.out.println("cydeoLink.isDisplayed() =" + cydeoLink.isDisplayed());
     }
 }
