b4bbeb282be29f5fdd242bbf96bdc2a4c0615e8f
Iremgunes <aviremdanacioglu@gmail.com>
2022-05-19 15:37:54 +0300

    19 may commit 1

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T1_StaleElementRefEx.java
@@ -0,0 +1,41 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_StaleElementRefEx {
+    public static void main(String[] args) throws InterruptedException {
+        // Open Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2. Go to https://practice.cydeo.com/add_remove_elements/
+        driver.get("https://practice.cydeo.com/add_remove_elements/");
+
+        //     3. Click to “Add Element” button
+        WebElement addElementButton = driver.findElement(By.xpath("//button[.='Add Element']"));
+
+        Thread.sleep(2000);
+        addElementButton.click();
+
+       //4. Verify “Delete” button is displayed after clicking.
+        WebElement deleteButton = driver.findElement(By.xpath("//button[@class='added-manually']"));
+
+
+
+//5. Click to “Delete” button.
+//6. Verify “Delete” button is NOT displayed after clicking.
+
+
+
+
+    }
+
+
+
+}
