82d6c66d0d7cbe0a09820d37a725e2daf1caf37f
Iremgunes <aviremdanacioglu@gmail.com>
2022-05-29 20:27:54 +0300

    29 may commit 4

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -1,6 +1,11 @@
 package com.cydeo.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
@@ -16,5 +21,40 @@ public class Driver {
      we make it static because we will use it in static method
      */
     private static WebDriver driver;
+
+    /*
+    create a reuasable utility method which will return same driver instance when we call it
+     */
+    public static WebDriver getDriver(){
+
+        if(driver==null){
+
+            /*
+            we read our browserType from configuration.properties.
+            this way, we can control which browser is opened from outside our code from configuration.properties.
+             */
+            String browserType = ConfigurationReader.getProperty("browser");
+
+            switch (browserType){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+            }
+
+
+        }
+        return driver;
+    }
 
 }
