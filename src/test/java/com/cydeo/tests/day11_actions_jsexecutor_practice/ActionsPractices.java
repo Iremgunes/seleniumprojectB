c348bedae60b3a68766eff025d88a39adf253733
Iremgunes <aviremdanacioglu@gmail.com>
2022-06-08 13:04:39 +0300

    8 jun commit 1

---
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
@@ -0,0 +1,41 @@
+package com.cydeo.tests.day11_actions_jsexecutor_practice;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class ActionsPractices {
+
+    @Test
+    public void task4_and_5_test(){
+
+       // 1- Open a chrome browser
+        //2- Go to: https://practice.cydeo.com/
+
+        Driver.getDriver().get("https://practice.cydeo.com/");
+
+        //3- Scroll down to “Powered by CYDEO”
+
+        Actions actions = new Actions(Driver.getDriver());
+
+        WebElement cydeoLink = Driver.getDriver().findElement(By.linkText("CYDEO"));
+
+        actions.moveToElement(cydeoLink).perform();
+
+
+        //4- Scroll using Actions class “moveTo(element)” method
+
+
+
+        actions.sendKeys(Keys.PAGE_UP).perform();
+
+
+
+
+
+
+    }
+}
