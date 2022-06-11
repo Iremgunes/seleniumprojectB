718ec583676fa34238ca5ab2e075b4a7b8ec6b3b
Iremgunes <aviremdanacioglu@gmail.com>
2022-06-11 19:23:49 +0300

    11 jun commit 3

--- src/test/java/com/cydeo/reviewWithOscar/week06/WaitExamples.java
+++ src/test/java/com/cydeo/reviewWithOscar/week06/WaitExamples.java
@@ -3,6 +3,8 @@ package com.cydeo.reviewWithOscar.week06;
 import com.cydeo.reviewWithOscar.week06.pomForReview.WaitTaskPage;
 import com.cydeo.utilities.Driver;
 import com.cydeo.utilities.ReviewUtils;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -10,9 +12,17 @@ import org.testng.annotations.Test;
 
 public class WaitExamples {
 
+    WebDriverWait wait;
+    WaitTaskPage page;
+
+
+
     @BeforeMethod
     public void setUp(){
         Driver.getDriver().get("https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver");
+         wait = new WebDriverWait(Driver.getDriver(), 10);
+        page = new WaitTaskPage();
+
     }
 
       //1. Go to https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver
@@ -26,10 +36,12 @@ public class WaitExamples {
 
     @Test
     public void example_tasks(){
-        WaitTaskPage page = new WaitTaskPage();
-        page.alertActivate.click();
 
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        page.alertActivate.click();
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = Driver.getDriver().switchTo().alert();
+        alert.accept();
+
 
 
 
